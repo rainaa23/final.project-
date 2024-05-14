7b734b474e0fab3fe524765bfeb69ea9275930f5
rainaa23 <146867466+rainaa23@users.noreply.github.com>
2024-05-14 14:59:12 +0530

    Delete signin and sign up page directory

--- signin and sign up page/script.js
+++
@@ -1,9 +0,0 @@
-const signUpBtnLink = document.querySelector('.signUpBtn-link');
-const signInBtnLink = document.querySelector('.signInBtn-link');
-const wrapper = document.querySelector('.wrapper');
-
-signUpBtnLink.addEventListener('click',() =>{
-    wrapper.classList.toggle('active');});
-
-signInBtnLink.addEventListener('click',() =>{
-    wrapper.classList.toggle('active');});
